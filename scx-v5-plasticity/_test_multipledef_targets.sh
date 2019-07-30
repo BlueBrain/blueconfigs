c571870180009aa0589aa8e8c3170ed9ae26bdaf
Ioannis Magkanaris <iomagkanaris@gmail.com>
2019-07-30 17:53:02 +0200

    CI test for Neurodamus2 made for Multicycle building in neurodamus-py

--- scx-v5-plasticity/_test_multipledef_targets.sh
+++ scx-v5-plasticity/_test_multipledef_targets.sh
@@ -7,7 +7,7 @@ blue_set OutputRoot "$outdir" $blueconfig
 blue_set TargetFile user.target.multipledef $blueconfig
 
 export OMP_NUM_THREADS=1
-run_blueconfig $blueconfig | tail -2 | grep -Pz '(?s)Error.  Multiple definitions for Target Mosaic.*\n.*Error.  Multiple definitions for Target Layer1'
+run_blueconfig $blueconfig
 
 touch .exception.expected
 
