c9eb85e7e39218a5b5a5e735fd1eed1bcbccd8bb
Ioannis Magkanaris <iomagkanaris@gmail.com>
2019-11-11 17:22:57 +0100

    Added test for build-model and simulate-model
    Change-Id: I40700d7bff4a30e0fd8d08b3460905e2df61f1eb

---
+++ quick-hip-sonata/test_coreneuron_model_building_py.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+module load python-dev
+module list
+
+source ../toolbox.sh
+configfile_bk $1
+outputdir=$2
+
+blue_set Simulator CORENEURON $blueconfig
+
+export OMP_NUM_THREADS=1
+RUN_PY_TESTS=yes run_blueconfig $blueconfig "--build-model"
+
+RUN_PY_TESTS=yes run_blueconfig $blueconfig "--simulate-model"
+
