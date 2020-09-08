050265cd893730d501673cd8a22c1e289afc2857
Omar Awile <omar.awile@epfl.ch>
2020-09-08 16:45:00 +0200

    Add a test for new delayed connections
    Delayed connection weight adjustment has been reimplemented in MOD
    such that it is now compatible with coreneuron. At the same time
    the semantic of "weight" has been fixed for the case of delayed
    connections, as it now correctly scales the synapse conductance,
    rather than resetting it to the value of the weight.
    
    This change adds a small test for this feature.
    
    NEURODAMUS_HIPPOCAMPUS_BRANCH=sandbox/awile/review_49688
    NEURODAMUS_NEOCORTEX_BRANCH=sandbox/awile/review_50245
    NEURODAMUS_MOUSIFY_BRANCH=sandbox/awile/review_50272
    NEURODAMUS_THALAMUS_BRANCH=sandbox/awile/review_50274
    NEURODAMUS_CORE_BRANCH=sandbox/awile/review_49687
    PYNEURODAMUS_BRANCH=sandbox/weji/delay_connection
    
    Change-Id: Iae6b17196a9d3bdec3ef13d51eafa0737bddf984

---
+++ quick-hip-delayconn/test_coreneuron.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+source ../toolbox.sh
+configfile_bk $1
+outputdir=$2
+
+blue_set Simulator CORENEURON $blueconfig
+
+export OMP_NUM_THREADS=1
+n=3 run_blueconfig $blueconfig
+
