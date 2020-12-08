b847361197c4895424afd1a0ca2c16d4e29a2cef
Weina Ji <weina.ji@epfl.ch>
2020-12-08 18:16:40 +0100

    New test in quick-v5-plasticity for SyncDelayOverride
    PYNEURODAMUS_BRANCH=sandbox/weji/SynDelayOverride
    
    Change-Id: Ice448be5c180f4b64ee8ad990ccaf8beab14efec

---
+++ quick-v5-plasticity/test_syndelayoverride.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source ../toolbox.sh
+configfile_bk $1
+outputdir=${2:-output}
+
+echo ">> Test SynDelayOverride in Connection block"
+
+blue_comment_section Report $blueconfig
+blue_set SynDelayOverride 1. $blueconfig "Connection ConL6Exc-Uni"
+blue_set SynDelayOverride 2. $blueconfig "Connection ConInh-Uni"
+
+run_blueconfig $blueconfig
+
+# check against different reference spikes
+echo "out.SynDelayOverride.sorted" > $outputdir/ref_spikes.txt
