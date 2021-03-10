f95edebf28457146b7f68fa0a1e65ddcd185ba94
Fernando Pereira <fernando.pereira@epfl.ch>
2021-03-10 15:42:52 +0100

    .jenkins.sh old tests to run only quick tests
    ENV_VARS:DRY_RUN=1
    
    Change-Id: Ib899af9241a0afce7fe78539164e5ddb39235e65

--- .jenkins.sh
+++ .jenkins.sh
@@ -4,7 +4,7 @@ source .jenkins/envutils.sh
 
 # In jenkins mode set HOME to BUILD_HOME
 export WORKSPACE=${WORKSPACE:-$_THISDIR}
-export HOME=$WORKSPACE/BUILD_HOME
+export HOME="$WORKSPACE/BUILD_HOME"
 
 # Prepare environment / spack
 _THISDIR=$(readlink -f $(dirname $BASH_SOURCE))
@@ -13,7 +13,11 @@ source $_THISDIR/.tests_setup.sh
 # Build req versions
 install_neurodamus
 
-run_all_tests
+if [ "$QUICK_TESTS" ]; then
+    run_quick_tests
+else
+    run_all_tests
+fi
 
 set +ex
 log "ALL TESTS PASSED" "SUCCESS" $Green
