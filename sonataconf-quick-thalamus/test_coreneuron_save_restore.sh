19394dad05e2190d2376cedeafefedc52e54338a
Weina Ji <weina.ji@epfl.ch>
2023-01-09 14:24:53 +0100

    Add coreneuron save restore in SONATA tests

---
+++ sonataconf-quick-thalamus/test_coreneuron_save_restore.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source ../toolbox.sh
+echo $1
+sonata_configfile_bk $1
+
+sonataconf_set "target_simulator" "CORENEURON" "$sonataconfig"
+echo "Run CORENEURON save with $sonataconfig"
+n=1 run_blueconfig $sonataconfig --output-path=save --save=save/checkpoint
+
+echo "Run CORENEURON restore with $sonataconfig"
+n=1 run_blueconfig $sonataconfig --restore=save/checkpoint
+#copy report from save folder for the result check as save has run the full duration
+output_dir=$(less $sonataconfig |grep -o '"output_dir":\s*"[^"]*' | grep -o '[^"]*$')
+cp save/*.h5 $output_dir/
