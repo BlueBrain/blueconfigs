45ff3ba601816af095581e90ea6391cd0b8cccc6
Nicolas Cornu <nicolas.cornu@epfl.ch>
2023-03-21 17:11:23 +0100

    Print statistics about failure

--- ci/compare_sonata_reports.py
+++ ci/compare_sonata_reports.py
@@ -70,12 +70,23 @@ for pop_name, pop_name2 in zip(elements.get_population_names(), elements2.get_po
         # Check all populations
         continue
     else:
+        nodes = 0
+        full_diff = numpy.array([])
         for node_id in node_ids:
             df_values = df[node_id].values
             df2_values = df2[node_id].values
             row_names = df[node_id].index
             if not numpy.allclose(df_values, df2_values):
+                nodes += 1
                 print(f">>>> Different values for node id '{node_id}' in population '{pop_name}':")
+                diff = numpy.fabs(df_values - df2_values)
+                full_diff = numpy.append(full_diff, diff)
+                size = numpy.size(diff)
+                nz = numpy.size(numpy.nonzero(diff))
+                print(f">>>>\t size: {size}, diff: {nz} ({nz/size*100}%)")
+                print(f">>>>\t max: {numpy.max(diff)}")
+                print(f">>>>\t avg: {numpy.average(diff)}")
+                print(f">>>>\t std: {numpy.std(diff)}")
                 # Loop through timesteps
                 for i, timestep in enumerate(df_values):
                     if not numpy.allclose(timestep, df2_values[i]):
@@ -84,6 +95,14 @@ for pop_name, pop_name2 in zip(elements.get_population_names(), elements2.get_po
                             if not numpy.allclose(element_value, df2_values[i][j]):
                                 print("[{:g}(ms)] ref {:.8f} vs output {:.8f} for element_id index {:d}".
                                     format(row_names[i], element_value, df2_values[i][j], j))
+        if nodes != 0:
+            print(f">>>> # nodes: {len(node_ids)}, failed nodes: {nodes}")
+            size = numpy.size(full_diff)
+            nz = numpy.size(numpy.nonzero(full_diff))
+            print(f">>>>\t size: {size}, diff: {nz} ({nz/size*100:.2}%)")
+            print(f">>>>\t max: {numpy.max(full_diff)}")
+            print(f">>>>\t avg: {numpy.average(full_diff)}")
+            print(f">>>>\t std: {numpy.std(full_diff)}")
         # Exit with error on the first population that fails
         exit(-1)
     exit(0)
